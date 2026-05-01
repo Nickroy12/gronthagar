5a5421e052d4e7e248ee6972faf9354ad5c640f3
Nick-roy <nroy08232@gmail.com>
2026-05-01 12:27:07 +0600

    details page done

--- ui/DetailsCard.jsx
+++ ui/DetailsCard.jsx
@@ -6,18 +6,23 @@ const DetailsCard = ({ book }) => {
 
   return (
     <div className="w-10/12 mx-auto shadow p-2 ">
-      <div className="flex gap-4">
+      <div className="lg:flex  gap-4">
         <div>
           <Image
             src={book.image_url}
             width={370}
             height={370}
             alt={book.title}
-            className="rounded-lg"
+            className="rounded-lg border-1 border-purple-500"
           />
         </div>
         <div className="space-y-3">
-          <h2 className="text-3xl text-purple-500 font-bold">{book.title}</h2>
+          <div className="flex justify-between mt-3">
+            <h2 className="text-3xl text-purple-500 font-bold">{book.title}</h2>
+            <div className="badge text-white bg-linear-to-r from-purple-500 to-purple-400">
+              {book.category}
+            </div>
+          </div>
           {book.author && (
             <p>
               {" "}
@@ -25,6 +30,10 @@ const DetailsCard = ({ book }) => {
             </p>
           )}
           <p>{book.description}</p>
+          <div className="flex justify-between">
+            <h3 className="text-xl text-gray-600"> In stock <span className="text-xl font-bold text-purple-500">{book.available_quantity}</span></h3>
+            <button className="btn rounded-4xl bg-linear-to-r from-purple-500 to-purple-400 text-white">Borrow Now</button>
+          </div>
         </div>
       </div>
     </div>
